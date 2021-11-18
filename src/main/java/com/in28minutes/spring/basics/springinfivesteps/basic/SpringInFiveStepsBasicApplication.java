60cdf0a353fa0431dfd013f2661abc58522b330a
Yoav <yoav.meron11@gmail.com>
2021-11-18 16:53:52 +0200

    Created SpringInFiveStepsBasicApplication class in basic package.

--- src/main/java/com/in28minutes/spring/basics/springinfivesteps/basic/SpringInFiveStepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springinfivesteps/basic/SpringInFiveStepsBasicApplication.java
@@ -1,12 +1,11 @@
-package com.in28minutes.spring.basics.springinfivesteps;
+package com.in28minutes.spring.basics.springinfivesteps.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class SpringInFiveStepsApplication {
+public class SpringInFiveStepsBasicApplication {
 
     //What are the beans?
     //What are the dependencies for the beans?
@@ -16,9 +15,14 @@ public class SpringInFiveStepsApplication {
         //BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
         
         //Application Context
-        ApplicationContext applicationContext = SpringApplication.run(SpringInFiveStepsApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SpringInFiveStepsBasicApplication.class, args);
 
         BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+
+        BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+
+        System.out.println(binarySearch);
+        System.out.println(binarySearch1);
 
         int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
         System.out.println(result);
