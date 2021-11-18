363c01b41e1e5e169eee99505c40519c52ac3810
Yoav <yoav.meron11@gmail.com>
2021-11-18 16:50:46 +0200

    Added scope functionality.

--- src/main/java/com/in28minutes/spring/basics/springinfivesteps/basic/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springinfivesteps/basic/BinarySearchImpl.java
@@ -1,10 +1,13 @@
-package com.in28minutes.spring.basics.springinfivesteps;
+package com.in28minutes.spring.basics.springinfivesteps.basic;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 
     @Autowired
